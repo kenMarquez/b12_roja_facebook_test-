2aa83b5921324f4d505d7220432037000f72cb64
KenMarquez <darius_dam@hotmail.com>
2016-11-24 20:28:52 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,4 @@
+from face_test_2.usuario import Usuario
+
+user = Usuario()
+json = user.get_info()
---
+++ usuario.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+
+class Usuario:
+    nombre = ""
+
+    def __init__(self):
+        self.url = "https://graph.facebook.com/v2.8/me"
+        self.access_token = "EAACEdEose0cBAPZAO8boT8vxgcj0qFpZAZC5IHuVZAJYa6h514357FyZB4akyIruKH1Se9ZBGtAcIqSjOQQXN647vsw4badlDi8frbbdqlAFzXuvlmKZBrgLFiZCyJRnqAcFRD01xDJKPYYafqZBtOMTZA5kAJjZBxlCiA13fhYnW3ZB8AZDZD"
+
+    def get_info(self):
+        params = {"access_token": self.access_token}
+        resultado = requests.get(self.url, params=params).json()
+        print(resultado)
+        self.nombre = resultado["name"]
+        return resultado
+
+
+
